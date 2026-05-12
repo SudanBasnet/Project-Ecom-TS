589419cc9dab3e8c1773af427b9266f4b9759190
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-05-12 21:59:39 +1000

    setting up automatic run and exec without build

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
 import express from "express";
+
+//!creating express app instance
+const app = express();
+
+//!body parser
+app.use(express.json({ limit: "10mb" }));
+
+//!using middlewares
+
+//!using routes
+
+//!error handler
+
+export default app;
