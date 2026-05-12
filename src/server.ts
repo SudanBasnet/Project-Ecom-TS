8e76edc6ec3f07e980d670cd8c08a3c7120ce78c
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-05-12 22:19:40 +1000

    database setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
+import connectDatabase from "./config/db.config";
 
 const PORT = 8080;
+const DB_URI = "mongodb://localhost:27017/team_12";
 
-//connect database
-
-//listening to port
+//! connect database
+connectDatabase(DB_URI);
+//! listening to port
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
