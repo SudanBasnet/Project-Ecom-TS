77441c4bf4bdf932e44487b943155dc9d9700d45
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-05-31 22:09:59 +1000

    filter: perPage,currPage,skip features

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -9,13 +9,55 @@ import {
   deleteFileFromCloudinary,
   sendFileToCloudinary,
 } from "../utils/cloudinary.utils";
+import mongoose from "mongoose";
 
 const folder = "/products";
 
 //* get all products
 export const getAll = catchAsync(async (req: Request, res: Response) => {
-  const filter = {};
+  const {
+    query,
+    category,
+    brand,
+    minPrice,
+    maxPrice,
+    limit = "10",
+    page = "1",
+  } = req.query;
+  const perPage = Number(limit);
+  const currPage = Number(page);
+  const filter: mongoose.QueryFilter<any> = {};
+  if (query) {
+    filter.$or = [
+      {
+        name: { $regex: query, $options: "i" },
+      },
+      {
+        description: { $regex: query, $options: "i" },
+      },
+    ];
+  }
+  if (category) {
+    filter.category = category;
+  }
 
+  if (brand) {
+    filter.brand = brand;
+  }
+
+  //! price filter
+  if (minPrice || maxPrice) {
+    if (minPrice) {
+      filter.price = {
+        $gte: Number(minPrice),
+      };
+    }
+    if (maxPrice) {
+      filter.price = {
+        $lte: Number(maxPrice),
+      };
+    }
+  }
   const products = await Product.find(filter);
 
   sendResponse(res, {
